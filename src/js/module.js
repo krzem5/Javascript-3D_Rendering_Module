6d06608c935aa1afb420629af562f7d5343aa168
Krzem <krzem5.dev@gmail.com>
2021-04-06 11:45:43 +0200

    Push Update 04/06/2021, 11:45:32

--- src/js/module.js
+++ src/js/module.js
@@ -1,8 +1,8 @@
 function color(r,g,b,a){
 	function hex(n){
-		if (typeof n==="string"||n instanceof String){if (n.length==2){return n};return "0"+n}
+		if ((typeof n==="string")||(n instanceof String)){if (n.length==2){return n};return "0"+n}
 		if (-1<n<256){if (n.toString(16).length==2){return n.toString(16)};return "0"+n.toString(16)}
-		return "00" 
+		return "00"
 	}
 	if (r!=undefined&&g==undefined&&b==undefined&&a==undefined&&typeof r=="string"){
 		return r
@@ -371,9 +371,9 @@ class TransformMatrix extends _Mat{
 		this._t.blank=function(){return [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]}
 		this._t.translate=function(a,b,c){return [[1,0,0,a],[0,1,0,b],[0,0,1,c],[0,0,0,1]]}
 		this._t.scale=function(a,b,c,fa,fb,fc){fa=fa||0;fb=fb||0;fc=fc||0;return [[a,0,0,fa*(1-a)],[0,b,0,fb*(1-b)],[0,0,c,fc*(1-c)],[0,0,0,1]]}
-		this._t.rotX=function(a,fa,fb,fc){fa=fa||0;fb=fb||0;fc=fc||0;a=radians(a);return [[1,0,0,0],[0,cos(a),-sin(a),fb*(1-cos(a))+fc*sin(a)],[0,sin(a),cos(a),fc*(1-cos(a))+fb*sin(a)],[0,0,0,1]]}
-		this._t.rotY=function(a,fa,fb,fc){fa=fa||0;fb=fb||0;fc=fc||0;a=radians(a);return [[cos(a),0,-sin(a),fa*(1-cos(a))+fc*sin(a)],[0,1,0,0],[sin(a),0,cos(a),fc*(1-cos(a))+fa*sin(a)],[0,0,0,1]]}
-		this._t.rotZ=function(a,fa,fb,fc){fa=fa||0;fb=fb||0;fc=fc||0;a=radians(a);return [[cos(a),-sin(a),0,fa*(1-cos(a))+fb*sin(a)],[sin(a),cos(a),0,fb*(1-cos(a))+fa*sin(a)],[0,0,1,0],[0,0,0,1]]}
+		this._t.rotX=function(a,fa,fb,fc){fb=fb||0;fc=fc||0;a=radians(a);return [[1,0,0,0],[0,cos(a),-sin(a),fb*(1-cos(a))+fc*sin(a)],[0,sin(a),cos(a),fc*(1-cos(a))+fb*sin(a)],[0,0,0,1]]}
+		this._t.rotY=function(a,fa,fb,fc){fa=fa||0;fc=fc||0;a=radians(a);return [[cos(a),0,-sin(a),fa*(1-cos(a))+fc*sin(a)],[0,1,0,0],[sin(a),0,cos(a),fc*(1-cos(a))+fa*sin(a)],[0,0,0,1]]}
+		this._t.rotZ=function(a,fa,fb,fc){fa=fa||0;fb=fb||0;a=radians(a);return [[cos(a),-sin(a),0,fa*(1-cos(a))+fb*sin(a)],[sin(a),cos(a),0,fb*(1-cos(a))+fa*sin(a)],[0,0,1,0],[0,0,0,1]]}
 		this.dt=dt.slice()
 		this._rc()
 	}
